40e8c2b4b70881ebd43452833e66df190f1c7848
C.Liang <lalatokijp@gmail.com>
2022-11-22 15:46:19 +0900

    #27_init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // React
 import React from 'react';
-
+// import React, { useState }  from 'react';
 // Amplify
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
@@ -8,26 +8,32 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 
 // Components
-import UsersDemo from './components/UsersDemo';
-import FoodTypesDemo from './components/FoodTypesDemo';
-import ActivitiesDemo from './components/ActivitiesDemo';
+// import UsersDemo from './components/UsersDemo';
+// import FoodTypesDemo from './components/FoodTypesDemo';
+// import ActivitiesDemo from './components/ActivitiesDemo';
+import ActivitiesCreate from './components/ActivitiesCreate';
 
 Amplify.configure(awsExports);
 
 // lint err function App(signOut, user) {
 function App() {
+  // const [userName, setUserName] = useState(user.username);
+
   return (
     <>
       <div className="App">
         <header className="App-header">
           <Authenticator socialProviders={['google']}>
             {({ signOut, user }) => (
+
               <main>
                 <h1>Hello {user.username}</h1>
                 <button onClick={signOut}>Sign out</button>
-                <UsersDemo />
-                <FoodTypesDemo />
-                <ActivitiesDemo />
+                {/* <UsersDemo /> */}
+                {/* <FoodTypesDemo />
+                <ActivitiesDemo /> */}
+                <ActivitiesCreate 
+                  user = { user } />
               </main>
             )}
           </Authenticator>
---
+++ src/components/ActivitiesCreate.jsx
@@ -0,0 +1,217 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Flex,
+  TextField,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+} from '@aws-amplify/ui-react';
+import {
+  listActivitiesUtil,
+  createActivitiesUtil,
+  // updateActivitiesUtil,
+  // deleteActivitiesUtil,
+} from '../utils/requestActivities';
+import {
+  listUsersUtil,
+  updateUsersUtil,
+} from '../utils/requestUsers';
+
+const ActivitiesCreate = (props) => {
+  const [activities, setActivities] = useState([]);
+  const [points, setPoints] = useState(0);
+
+  // 2.1 show ZaifuPoint on screen
+  useEffect(() => {
+    (async () => {
+      const initPoint = await getUserPoint(userInfo.user.username);
+      console.log("initPoint", initPoint);
+      setPoints(initPoint);  
+    })();
+  }, []);
+  console.log("activities", activities);
+
+  // 1. get userId
+  const userInfo = props;
+  const userName = userInfo.user.username;
+  console.log("user from props", userInfo.user.username, userName);
+
+  // 2. get user ZaifuPoint by given userId
+  const getUserPoint = async (userId) =>  {
+    // console.log("userId", userId);
+    // const userList = await listUsersUtil();
+    const userPoint = await listUsersUtil().then(
+      (res) => res.find(user => user.userId === userId).zaifuPoint
+    );
+    // console.log("userId, userPoint", userId, userPoint);
+    // console.log("point", userList.find(user => user.userId === 'lalatoki'));
+    setPoints(userPoint);
+    return userPoint;
+  };
+
+  // 3. get user category(walking - amount) VALUE from screen
+  const getAmount = (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const amt = form.get('amount')
+    console.log("-------- amt --------". amt)
+    return amt;
+  };
+
+  // 4.  calculate ZaifuPoint by give VALUE
+  const calcZaifuPoint = (amount) => {
+    const weight = 0.1;
+    const zaifuPoint = Math.round(amount * weight);
+    return zaifuPoint;
+  };
+  console.log("calcZaifuPoint", calcZaifuPoint);
+
+  // 5. update ZaifuPoint by given userID & diff point
+  const updateUserPoint = async (userId, zaifuPoint) => {
+    // get user
+    const user = await listUsersUtil().then(
+      (res) => res.find(user => user.userId === userId)
+      );
+    // update user point
+    user.zaifuPoint = zaifuPoint;
+    // update user
+    const newUsers = await updateUsersUtil(user);
+    console.log("newUsers", newUsers);
+    // console.log("newUsers.find().zaifuPoint;", newUsers.find(user => user.userId === user.userId));
+    // return newUsers.find(user => user.userId === user.userId).zaifuPoint;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const activitiesList = await listActivitiesUtil();
+      setActivities(activitiesList);
+    })();
+  }, []);
+
+  const createActivity = async (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const data = {
+      userId: form.get('userId'),
+      activityId: form.get('activityId'),
+      amount: form.get('amount'),
+    };
+    const newActivities = await createActivitiesUtil(data);    
+    setActivities(newActivities);
+  };
+
+  return (
+    <>
+      <h1>Create Activities</h1>
+      <div>
+        {/* 
+        1. get userId
+        2. get user ZaifuPoint by given userId
+        2.1 show ZaifuPoint on screen
+        3. get user category(walking - amount) VALUE from screen
+        4.  calculate ZaifuPoint by give VALUE
+        5. update ZaifuPoint by given userID & diff point
+        6. calc ZaifuPoint diff (+ xxx point plused) and show on screen
+        7. change SAVE button to BACK button 
+        */}
+      </div>
+      <div>
+        {/* <h2>Create Activity</h2> */}
+        <View
+          as="form"
+          onSubmit={(e) => {
+            createActivity(e);
+            // console.log("------ amount ------", FormData(e.target).get('amount'));
+            getAmount(e);
+            updateUserPoint(userName, 112 );            
+          }}
+        >
+          <Flex direction="column" alignItems="left">
+            <TextField
+              name="userId"
+              placeholder="XXXXX"
+              label="userId"
+              variation="default"
+              required
+            />
+            <TextField
+              name="activityId"
+              placeholder="walk"
+              label="activityId"
+              variation="default"
+              required
+            />
+            <TextField
+              name="amount"
+              placeholder="60"
+              label="amount"
+              variation="default"
+              required
+            />
+          </Flex>
+          <br />
+          <Button type="submit" variation="primary">
+            Create Activity
+          </Button>
+
+        </View>
+        <div>
+        ZaifuPoint: &nbsp; { points } <br></br>
+        walking VALUE: &nbsp; <br></br>
+        ZaifuPoint by VALUE: &nbsp; 
+        </div>
+        <Button onClick={ (e) => {
+            console.log("e", e);
+            // console.log("getUserPoint", getUserPoint('lalatoki'));
+            console.log("getUserPoint", getUserPoint(userName));
+            // console.log("------ amount ------", FormData(e.target).get('amount'));
+            // console.log("------ amount", document.getElementsByName('amount').value);
+          }}>test</Button>
+
+      </div>
+
+      <div>
+        <h2>list Activity</h2>
+        <Table caption="" highlightOnHover={false}>
+          <TableHead>
+            <TableRow>
+              <TableCell as="th">userId</TableCell>
+              <TableCell as="th">activityId</TableCell>
+              <TableCell as="th">amount</TableCell>
+              <TableCell as="th">createdAt</TableCell>
+              <TableCell as="th">updatedAt</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {activities.map((activity) => (
+              <TableRow key={activity.id}>
+                <TableCell key={activity.userId + 'userId'}>
+                  {activity.userId}
+                </TableCell>
+                <TableCell key={activity.activityId + 'activityId'}>
+                  {activity.activityId}
+                </TableCell>
+                <TableCell key={activity.amount + 'amount'}>
+                  {activity.amount}
+                </TableCell>
+                <TableCell key={activity.createdAt + 'createdAt'}>
+                  {activity.createdAt}
+                </TableCell>
+                <TableCell key={activity.updatedAt + 'updatedAt'}>
+                  {activity.updatedAt}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+    </>
+  );
+};
+
+export default ActivitiesCreate;
